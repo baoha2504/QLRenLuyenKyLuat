c1ac131c2bb401c014c0902e7ff6408c0d6405c3
baoha2504 <hacongquoclun1@gmail.com>
2022-09-28 16:03:18 +0700

    push code

---
+++ QLRenLuyenKyLuat/GUI_HV/frmHocVien.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLRenLuyenKyLuat.GUI_HV
+{
+    public partial class frmHocVien : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmHocVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
