9be900667a6c1ebd3cedbc66774f86229badf352
baoha2504 <hacongquoclun1@gmail.com>
2022-09-28 15:56:28 +0700

    push code

---
+++ QLRenLuyenKyLuat/GUI_LT/frmLopTruong.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLRenLuyenKyLuat.GUI_LT
+{
+    public partial class frmLopTruong : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmLopTruong()
+        {
+            InitializeComponent();
+        }
+    }
+}
