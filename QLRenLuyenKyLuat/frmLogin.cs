9c914f069876fdb94be5b36bae2b72b687ee9bc5
Bao <89384723+baoha2504@users.noreply.github.com>
2022-12-27 23:26:21 +0700

    bao commmit

--- QLRenLuyenKyLuat/frmLogin.cs
+++ QLRenLuyenKyLuat/frmLogin.cs
@@ -16,14 +16,17 @@ namespace QLRenLuyenKyLuat
         public static string position;
         public static string name;
         public static string lop;
-        public static string maHV;
-        DataTable dtbl;
+        public static string maNguoiDung;
+        public static string hashPass;
+        DataTable dtblDD;
+        DataTable dtblL;
+        DataTable dtblHV;
+        string query;
         public frmLogin()
         {
             InitializeComponent();
         }
-
-        private void guna2Button1_Click(object sender, EventArgs e)
+        public string ComputeSha256Hash(string rawData)
         {
             // Create a SHA256   
             using (SHA256 sha256Hash = SHA256.Create())
@@ -52,23 +55,64 @@ namespace QLRenLuyenKyLuat
             //sqlCon.Close();
             if (dtblDD.Rows.Count == 1)
             {
-                maHV = txtLoginMaHV.Text.Trim();
-                foreach (DataRow dr in dtbl.Rows)
+                maNguoiDung = txtLoginMaHV.Text.Trim();
+                foreach (DataRow dr in dtblDD.Rows)
                 {
                     name = dr["HoTen"].ToString();
                     position = dr["ChucVu"].ToString();
                     //lop = dr["MaLop"].ToString();
                 }
                 sqlCon.Close();
-                frmLop frm = new frmLop();
+                frmDaiDoi frm = new frmDaiDoi();
                 frm.Show();
                 this.Hide();
             }
             else
             {
-                MessageBox.Show("Check your Username and Password!");
+                query = "select * from LOP where MaLop = N'"+ txtLoginMaHV.Text.Trim() + "' and MatKhau = '"+ ComputeSha256Hash(txtLoginPass.Text.Trim()) + "'";
+                SqlDataAdapter daL = new SqlDataAdapter(query, sqlCon);
+                dtblL = new DataTable();
+                daL.Fill(dtblL);
+                if (dtblL.Rows.Count == 1)
+                {
+                    maNguoiDung = txtLoginMaHV.Text.Trim();
+                    foreach (DataRow dr in dtblL.Rows)
+                    {
+                        //name = dr["MaLop"].ToString();
+                        //position = dr["ChucVu"].ToString();
+                        lop = dr["MaLop"].ToString();
+                    }
+                    sqlCon.Close();
+                    frmLop frm = new frmLop();
+                    frm.Show();
+                    this.Hide();
+                } else
+                {
+                    query = "select * from HOCVIEN where MaHocVien = N'"+ txtLoginMaHV.Text.Trim() + "' and MatKhau = '"+ ComputeSha256Hash(txtLoginPass.Text.Trim()) + "'";
+                    SqlDataAdapter daHV = new SqlDataAdapter(query, sqlCon);
+                    dtblHV = new DataTable();
+                    daHV.Fill(dtblHV);
+                    if (dtblHV.Rows.Count == 1)
+                    {
+                        maNguoiDung = txtLoginMaHV.Text.Trim();
+                        foreach (DataRow dr in dtblHV.Rows)
+                        {
+                            name = dr["TenHocvien"].ToString();
+                            position = dr["ChucVu"].ToString();
+                            lop = dr["MaLop"].ToString();
+                        }
+                        sqlCon.Close();
+                        frmHocVien frm = new frmHocVien();
+                        frm.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Check your Username and Password!");
+                    }
+                }
             }
-
+            hashPass = ComputeSha256Hash(txtLoginPass.Text);
         }
     }
 }
